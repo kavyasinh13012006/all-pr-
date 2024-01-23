0be2a210579672b52d822833dc196f79e43ea666
kavyasinh13012006 <singhkavy66@gmail.com>
2024-01-23 12:20:51 +0530

    all pr

---
+++ Tailwind pr/assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  content: [],
+  theme: {
+    fontFamily:{
+      'inter': ['Inter', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'pink': '#FF3294',
+        'gray': '#E9E9E9',
+        'bg-gray': '#fafafa',
+        'gray2': '#dcdcdc',
+      },
+    },
+  },
+  plugins: [],
+}
+
